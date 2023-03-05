f1f720f64946064f175deed43b84fc09e61cc66d
kostia shestakov <100152765+mateese@users.noreply.github.com>
2023-03-05 02:25:49 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,71 +1,223 @@
-// form
-const createForm = document.forms.create;
-const formWrapper = document.querySelector('.form__wrapper');
-const submitButton = document.querySelector('.button__submit');
 
-// user block
-const userInfo = document.querySelector('.created');
-userInfo.style.display = 'none';
+"use strict";
 
-const signoutButton = document.querySelector('.button__signout');
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.subButton.addEventListener('click', formSend);
 
-const userName = document.querySelector('.created__name-surname');
-const userEmail = document.querySelector('.created__email');
-const userGender = document.querySelector('.created__icon img');
-const userPositions = document.querySelector('.created__position');
+    // =============== validation form ==========================================================================================================================
+    async function formSend(e) {
 
-// disabled button while checkbox not checked
-createForm.buttonSubmit.disabled = true;
-createForm.agree.addEventListener('click', function (event) {
-    if (createForm.agree.checked) {
-        submitButton.style.backgroundColor = '#37B16D';
-        createForm.buttonSubmit.disabled = false;
+        e.preventDefault();
+
+        let error = formValidate(form);
+
+        if (!error) {
+            const userName = form.userName;
+            const userSurname = form.userSurname;
+            const userEmail = form.userEmail;
+            const userGender = form.userGender;
+            const userAge = form.userAge;
+            const userDescription = form.userDescription;
+            const userLanguage = form.language;
+            const userImage = form.userImage;
+
+            const valueUserName = userName.value;
+            const valueUserSurname = userSurname.value;
+            const valueUserEmail = userEmail.value;
+            const valueUserGender = userGender.value;
+            const valueUserAge = userAge.value;
+            const valueUserDescription = userDescription.value;
+            const valueUserImage = userImage.files[0];
+            const defaultImage = document.querySelector('.default');
+
+            let fileUrl;
+            if (valueUserImage) {
+                fileUrl = URL.createObjectURL(valueUserImage);
+            } else if (defaultImage) {
+                fileUrl = defaultImage.innerHTML = `./img/user.jpg`;
+            }
+
+            const valueUserLanguageIndex = userLanguage.selectedIndex;
+            // const valueUserLanguage = userLanguage.value;
+            const valueUserLanguageValue = userLanguage.options[valueUserLanguageIndex].text;
+
+            const userInfoName = document.querySelector('.info__name');
+            const userInfoEmail = document.querySelector('.info__emailUser');
+            const userInfoGender = document.querySelector('.info__genderUser');
+            const userInfoAge = document.querySelector('.info__ageUser');
+            const userInfoDescription = document.querySelector('.desc__description');
+            const userInfoLanguage = document.querySelector('.info__lang');
+            const userInfoImage = document.querySelector('.info__image');
+
+            userInfoName.textContent = `${valueUserName} ${valueUserSurname}`;
+            userInfoEmail.textContent = `${valueUserEmail}`;
+            userInfoGender.textContent = `${valueUserGender}`;
+            userInfoAge.textContent = `${valueUserAge} year`;
+            userInfoDescription.textContent = `${valueUserDescription}`;
+            userInfoLanguage.textContent = `${valueUserLanguageValue}`;
+            userInfoImage.innerHTML = `<img src="${fileUrl}" alt="user photo">`;
+
+            const formBlock = document.querySelector('.account__form');
+            const infoBlock = document.querySelector('.info');
+            formBlock.style.display = 'none';
+            infoBlock.style.display = 'block';
+
+            const prew = document.querySelector('.file__preview img');
+            const backButton = document.querySelector('.info__button-back');
+            backButton.addEventListener('click', function (event) {
+                // prew.firstElementChild.remove();
+                form.subButton.disabled = true;
+                submitButton.style.cursor = 'not-allowed';
+                submitButton.style.backgroundColor = '#f06161b9';
+                formBlock.style.display = 'block';
+                infoBlock.style.display = 'none';
+                prew.style.display = 'none';
+                userInfoImage.innerHTML = `<img class="default" src="./img/user.jpg" alt="user photo">`;
+                form.reset();
+            });
+        }
+
+
+        //===== user information ======================================================================================================================================
+
+
+    };
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let i = 0; i < formReq.length; i++) {
+            const input = formReq[i];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+            input.addEventListener('focus', function (event) {
+                formRemoveError(input)
+            });
+        }
+        return error;
     }
-    else if (!createForm.agree.checked) {
-        submitButton.style.backgroundColor = '';
-        createForm.buttonSubmit.disabled = true;
+
+    function formAddError(input) {
+        input.previousElementSibling.classList.add('_error');
+        input.classList.add('_error');
     }
-});
+    function formRemoveError(input) {
+        input.previousElementSibling.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
 
-// submit form
-createForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    // get form value
-    const userFirstName = createForm.firstName.value;
-    const userSecondName = createForm.secondName.value;
-    const userValueEmail = createForm.userEmail.value;
-    const userValueGender = createForm.sex.value;
-    const getSelectedIndex = createForm.position.selectedIndex;
-    const userValuePosition = createForm.position[getSelectedIndex].text;
-    // assign this value
-    const profileName = `${userFirstName} ${userSecondName}`;
-    const profileEmail = `${userValueEmail}`;
-    userGender.src = userValueGender === 'man' ? './img/male-user.png' : './img/female-user.png';
-    const profilePosition = `${userValuePosition}`;
-    // inner this value
-    userName.innerHTML = profileName;
-    userEmail.innerHTML = profileEmail;
-    userPositions.innerHTML = profilePosition;
+    //================= check agree (checkbox) =======================================================================================================================================
 
-    // back to form
-    userInfo.style.display = 'block';
-    formWrapper.style.display = 'none';
+    const agreeCheck = document.form.userAgree;
+    const submitButton = document.querySelector('.button__submit');
+    const resetButton = document.querySelector('.button__reset');
 
-    signoutButton.addEventListener("click", function (event) {
-        userInfo.style.display = 'none';
-        formWrapper.style.display = 'block';
-        submitButton.style.backgroundColor = '';
-        createForm.buttonSubmit.disabled = true;
-        createForm.reset();
+    agreeCheck.addEventListener('click', checkAgree);
+    resetButton.addEventListener('click', checkReset);
+
+    function checkAgree() {
+        if (agreeCheck.checked) {
+            submitButton.removeAttribute('disabled');
+            submitButton.style.cursor = 'pointer';
+            submitButton.style.backgroundColor = '#F06162';
+        } else {
+            submitButton.setAttribute('disabled', '');
+            submitButton.style.cursor = 'not-allowed';
+            submitButton.style.backgroundColor = '#f06161b9';
+        }
+    }
+    function checkReset() {
+        const prew = document.querySelector('.file__preview img');
+        submitButton.setAttribute('disabled', '');
+        submitButton.style.cursor = 'not-allowed';
+        submitButton.style.backgroundColor = '#f06161b9';
+        prew.style.display = 'none';
+    }
+
+
+    // =========== add image =======================================================================================================================
+    const formImage = document.getElementById('formImage');
+    const formPreview = document.getElementById('formPreview');
+
+    formImage.addEventListener('change', () => {
+        uploadFile(formImage.files[0]);
+    });
+
+    function uploadFile(file) {
+        if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+            alert('Only image');
+            formImage.value = '';
+            return;
+        }
+        if (file.size > 2 * 1024 * 1024) {
+            alert('File can`t more 2 MB.');
+            return;
+        }
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            formPreview.innerHTML = `<img src="${e.target.result}" alt="userPhoto">`;
+        };
+        reader.onerror = function (e) {
+            alert('Error');
+        };
+        reader.readAsDataURL(file);
+    }
+
+    // ============== customize counter ===========================================================================================================================
+    jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
+    jQuery('.quantity').each(function () {
+        var spinner = jQuery(this),
+            input = spinner.find('input[type="number"]'),
+            btnUp = spinner.find('.quantity-up'),
+            btnDown = spinner.find('.quantity-down'),
+            min = input.attr('min'),
+            max = input.attr('max');
+
+        btnUp.click(function () {
+            var oldValue = parseFloat(input.val());
+            if (oldValue >= max) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue + 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
+        btnDown.click(function () {
+            var oldValue = parseFloat(input.val());
+            if (oldValue <= min) {
+                var newVal = oldValue;
+            } else {
+                var newVal = oldValue - 1;
+            }
+            spinner.find("input").val(newVal);
+            spinner.find("input").trigger("change");
+        });
+
     });
 });
 
-
-
-
-
-
-
+// =========================  ===========================================================================================================
 
 
 
